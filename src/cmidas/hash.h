a6d0bf041ee358f12e9e21ad3cb72db2de305658
Gabriel Ionescu <gabriel.nv.ionescu@gmail.com>
2017-08-17 19:24:44 +0100

    Ads minor corrections in hash explanation.

--- src/cmidas/hash.h
+++ src/cmidas/hash.h
@@ -10,12 +10,12 @@ struct hash
    resized.
 
    The table allocates a number of `slack` rows, that do not factor into its
-   decalred size. Meaning that is a table has a size of 32 and a slack of 5,
-   the initial insertion operation could only target rows 0 to 31. But, for
-   example, if it targets row 31 and finds it already occupied, it can continue
-   probing rows 32 to 36. Coupled with the fact that the threshold on probing
-   is precisely the number of slack rows means we can skip bounds checking when
-   inserting.                                                                */
+   declared size. For example, if a table has a size of 32 and a slack of 5,the
+   initial insertion operation could only target rows 0 to 31. But, if it
+   targets row 31 and finds it is already occupied, it can continue probing
+   rows 32 to 36. Coupled with the fact that the threshold on probing is
+   precisely the number of slack rows, this means we can skip bounds checking
+   when inserting.                                                           */
 {
     struct entry** table;
     int size;
